9cba91734f4b2df8efd4841b36476bc6507c077d
texasaggie97 <aet7yYhk7kgFqNn3>
2015-07-26 18:09:10 -0500

    Top 10 tracks activity now works, properly using the ContentProvider

--- app/src/main/java/com/markesilva/spotifystreamer/ArtistTracksActivityFragment.java
+++ app/src/main/java/com/markesilva/spotifystreamer/ArtistTracksActivityFragment.java
@@ -1,27 +1,35 @@
 package com.markesilva.spotifystreamer;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.markesilva.spotifystreamer.data.SpotifyContract;
+import com.markesilva.spotifystreamer.data.SpotifyProvider;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
@@ -33,13 +41,13 @@ import kaaes.spotify.webapi.android.models.Tracks;
 /**
  * Fragment for the top track view.
  */
-public class ArtistTracksActivityFragment extends Fragment {
+public class ArtistTracksActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
     final private String LOG_TAG = ArtistTracksActivityFragment.class.getSimpleName();
+    private static final int TRACK_LOADER = 1;
     static final String ARTIST_NAME = "artistname";
     static final String ARTIST_ID = "artistid";
     SpotifyApi mSpotifyApi = null;
     SpotifyService mSpotify = null;
-    List<TrackListRow> mTrackList = null;
     TrackListAdapter mTrackListAdapter = null;
     ListView mTrackListView = null;
     String mArtistName;
@@ -53,11 +61,25 @@ public class ArtistTracksActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_artist_tracks, container, false);
 
-        if (mTrackList == null) {
-            mTrackList = new ArrayList<>();
+        Bundle args = getArguments();
+        if (args != null) {
+            mArtistName = args.getString(ARTIST_NAME);
+            mArtistId = args.getString(ARTIST_ID);
+
+            Log.v(LOG_TAG, "Starting from intent: " + mArtistName);
+            AppCompatActivity a = ((AppCompatActivity) getActivity());
+            if (a != null) {
+                ActionBar b = a.getSupportActionBar();
+                if (b != null) {
+                    b.setSubtitle(mArtistName);
+                }
+            }
         }
 
-        mTrackListAdapter = new TrackListAdapter(getActivity(), mTrackList);
+        Uri artistUri = SpotifyContract.TrackEntry.buildTracksWithQuery(mArtistId);
+        Cursor cur = getActivity().getContentResolver().query(artistUri, null, null, null, null);
+
+        mTrackListAdapter = new TrackListAdapter(getActivity(), cur, 0);
 
         // Set up the adapter for the list view
         mTrackListView = (ListView) rootView.findViewById(R.id.track_list);
@@ -84,22 +106,7 @@ public class ArtistTracksActivityFragment extends Fragment {
             setRetainInstance(true);
         }
 
-        Bundle args = getArguments();
-        if (args != null) {
-            mArtistName = args.getString(ARTIST_NAME);
-            mArtistId = args.getString(ARTIST_ID);
-
-            Log.v(LOG_TAG, "Starting from intent: " + mArtistName);
-            AppCompatActivity a = ((AppCompatActivity) getActivity());
-            if (a != null) {
-                ActionBar b = a.getSupportActionBar();
-                if (b != null) {
-                    b.setSubtitle(mArtistName);
-                }
-            }
-            updateTrackList(mArtistId);
-        }
-
+        updateTrackList(mArtistId);
         return rootView;
     }
 
@@ -113,17 +120,49 @@ public class ArtistTracksActivityFragment extends Fragment {
     @Override
     @SuppressWarnings("unchecked")
     public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(TRACK_LOADER, null, this);
         super.onActivityCreated(savedInstanceState);
     }
 
-    private void updateTrackList(String artist) {
-        GetTrackInfoTask trackTask = new GetTrackInfoTask();
-        trackTask.execute(artist);
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        Uri trackListUri = SpotifyContract.TrackEntry.buildTracksWithArtistId(mArtistId);
+        Log.v(LOG_TAG, "Query Uri == " + trackListUri);
+        return new CursorLoader(getActivity(),
+                trackListUri,
+                null,
+                null,
+                null,
+                null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        Log.d(LOG_TAG, "Loader complete. " + cursor.getCount() + " records found");
+        mTrackListAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        mTrackListAdapter.swapCursor(null);
+    }
+
+    private void updateTrackList(String artistId) {
+        GetTrackInfoTask trackTask = new GetTrackInfoTask(getActivity(), artistId);
+        trackTask.execute(artistId);
+        getLoaderManager().restartLoader(TRACK_LOADER, null, this);
     }
 
     // We need to do the actual web query on a thread other than the UI thread. We use AsyncTask for this
     private class GetTrackInfoTask extends AsyncTask<String, Void, Tracks> {
         private final String LOG_TAG = GetTrackInfoTask.class.getSimpleName();
+        private Context mContext;
+        private String mArtistId;
+
+        public GetTrackInfoTask(Context context, String artisId) {
+            mContext = context;
+            mArtistId = artisId;
+        }
 
         protected Tracks doInBackground(String... artistId) {
             // If we didn;t get an artist ID then we have nothing to do
@@ -151,16 +190,95 @@ public class ArtistTracksActivityFragment extends Fragment {
             super.onPostExecute(tracks);
 
             if (tracks != null) {
-                mTrackList.clear();
+                // We found some artists. First we need to delete any records for this search
+                Cursor cursor = mContext.getContentResolver().query(
+                        SpotifyContract.SearchQueryEntry.CONTENT_URI,
+                        null,
+                        SpotifyProvider.sQueryStringSelection,
+                        new String[]{mArtistId},
+                        null);
+                if (cursor.getCount() > 0) {
+                    cursor.moveToFirst();
+                    int idx = cursor.getColumnIndex(SpotifyContract.SearchQueryEntry._ID);
+                    long queryId = cursor.getInt(idx);
+                    mContext.getContentResolver().delete(
+                            SpotifyContract.TrackEntry.CONTENT_URI,
+                            SpotifyContract.TrackEntry.TABLE_NAME + "." + SpotifyContract.TrackEntry.COLUMN_SEARCH_ID + " = ? ",
+                            new String[]{String.valueOf(queryId)});
+                }
+                cursor.close();
+
+                // Now we need to insert the new search query info
+                ContentValues queryValues = new ContentValues();
+                queryValues.put(SpotifyContract.SearchQueryEntry.COLUMN_QUERY_STRING, mArtistId);
+                long julianTime = System.currentTimeMillis();
+                queryValues.put(SpotifyContract.SearchQueryEntry.COLUMN_QUERY_TIME, julianTime);
+                Uri queryInsertUri = mContext.getContentResolver().insert(
+                        SpotifyContract.SearchQueryEntry.CONTENT_URI,
+                        queryValues);
+                long queryRowId = ContentUris.parseId(queryInsertUri);
+
+                // Now we need to get the row id of the associated artist item
+                String[] projection = {SpotifyContract.ArtistEntry.TABLE_NAME + "." + SpotifyContract.ArtistEntry._ID};
+                cursor = mContext.getContentResolver().query(
+                        SpotifyContract.ArtistEntry.buildArtistsWithArtistId(mArtistId),
+                        projection,
+                        null,
+                        null,
+                        null);
+                long artistRowId = -1;
+                if (cursor.getCount() > 0) {
+                    cursor.moveToFirst();
+                    int idx = cursor.getColumnIndex(SpotifyContract.ArtistEntry._ID);
+                    artistRowId = cursor.getInt(idx);
+                    Log.v(LOG_TAG, "Artist ID: " + artistRowId);
+                }
+                cursor.close();
+
+                Vector<ContentValues> cvVector = new Vector<>();
 
                 // for each track returned, add to the internal list
                 for (Track t : tracks.tracks) {
-                    TrackListRow rowItem = new TrackListRow(t.album.images, t.album.name, t.name, t.preview_url);
-                    mTrackList.add(rowItem);
+                    ContentValues c = new ContentValues();
+                    c.put(SpotifyContract.TrackEntry.COLUMN_SEARCH_ID, queryRowId);
+                    c.put(SpotifyContract.TrackEntry.COLUMN_ARTIST_ID, artistRowId);
+
+                    Image thumbnail = null;
+                    Image image = null;
+                    for (Image i: t.album.images) {
+                        // store the largest image for playback
+                        if ((image == null) || (i.height > image.height)) {
+                            image = i;
+                        }
+                        // store the smallest image that is still at least 75 pixels tall, for the list of tracks
+                        if ((thumbnail == null) || ((i.height < thumbnail.height) && (i.height >= 75))) {
+                            thumbnail = i;
+                        }
+                    }
+
+                    if (thumbnail != null) {
+                        c.put(SpotifyContract.TrackEntry.COLUMN_THUMBNAIL_URL, thumbnail.url);
+                    } else {
+                        c.put(SpotifyContract.TrackEntry.COLUMN_THUMBNAIL_URL, "");
+                    }
+                    if (image != null) {
+                        c.put(SpotifyContract.TrackEntry.COLUMN_IMAGE_URL, image.url);
+                    } else {
+                        c.put(SpotifyContract.TrackEntry.COLUMN_IMAGE_URL, "");
+                    }
+
+                    c.put(SpotifyContract.TrackEntry.COLUMN_ALBUM_NAME, t.album.name);
+                    c.put(SpotifyContract.TrackEntry.COLUMN_TRACK_NAME, t.name);
+                    c.put(SpotifyContract.TrackEntry.COLUMN_PREVIEW_URL, t.preview_url);
+
+                    cvVector.add(c);
                     Log.v(LOG_TAG, "Track = " + t.name);
                 }
-                mTrackListAdapter.setList(mTrackList);
-                mTrackListAdapter.notifyDataSetChanged();
+
+                ContentValues[] cvArray = new ContentValues[cvVector.size()];
+                cvVector.toArray(cvArray);
+                int inserted = mContext.getContentResolver().bulkInsert(SpotifyContract.TrackEntry.CONTENT_URI, cvArray);
+                Log.d(LOG_TAG, "GetTrackInfoTask complete. " + inserted + " records inserted");
             }
 
             if ((tracks == null) || (tracks.tracks.size() == 0)) {
@@ -169,124 +287,53 @@ public class ArtistTracksActivityFragment extends Fragment {
         }
     }
 
-    // Container for all information we need for top tracks for an artist
-    private class TrackListRow {
-        private String album;
-        private String track;
-        private String previewURL;
-        private Image image;
-        private Image thumbnail;
-
-        public TrackListRow(List<Image> images, String album, String track, String previewURL) {
-            this.image = null;
-            this.thumbnail = null;
-            for (Image i : images) {
-                // store the largest image for playback
-                if ((image == null) || (i.height > image.height)) {
-                    this.image = i;
-                }
-                // store the smallest image that is still at least 75 pixels tall, for the list of tracks
-                if ((thumbnail == null) || ((i.height < thumbnail.height) && (i.height > 75))) {
-                    this.thumbnail = i;
-                }
-            }
-            this.album = album;
-            this.track = track;
-            this.previewURL = previewURL;
-        }
-
-        // Get/Set
-        public Image getImage() {
-            return image;
-        }
-
-        public Image getThumbnail() {
-            return thumbnail;
-        }
-
-        public String getAlbum() {
-            return album;
-        }
-
-        public String getTrack() {
-            return track;
-        }
-
-        public String getPreviewURL() {
-            return previewURL;
-        }
-
-        @Override
-        public String toString() {
-            return album + "/" + track;
-        }
-    }
-
     // listview adapter for track list
-    private class TrackListAdapter extends BaseAdapter {
-        List<TrackListRow> mTrackList = null;
+    private class TrackListAdapter extends CursorAdapter {
         Context mContext = null;
 
-        public TrackListAdapter(Context c, List<TrackListRow> info) {
-            mContext = c;
-            mTrackList = info;
+        public TrackListAdapter(Context context, Cursor c, int flags) {
+            super(context, c, flags);
+            mContext = context;
         }
 
         // private view holder class
         private class ViewHolder {
             ImageView imageView;
+            int imageColId;
             TextView txtAlbum;
+            int albumColId;
             TextView txtTrack;
-        }
-
-        public void setList(List<TrackListRow> info) {
-            mTrackList = info;
+            int trackColId;
         }
 
         @Override
-        public int getCount() {
-            if (mTrackList != null) {
-                return mTrackList.size();
-            } else {
-                return 0;
+        public View newView(Context context, Cursor c, ViewGroup parent) {
+            View view = LayoutInflater.from(context).inflate(R.layout.track_listitem, parent, false);
+            ViewHolder holder = new ViewHolder();
+            holder.imageView = (ImageView) view.findViewById(R.id.track_listitem_image);
+            holder.imageColId = c.getColumnIndex(SpotifyContract.TrackEntry.COLUMN_THUMBNAIL_URL);
+            holder.txtAlbum = (TextView) view.findViewById(R.id.track_listitem_album);
+            holder.albumColId = c.getColumnIndex(SpotifyContract.TrackEntry.COLUMN_ALBUM_NAME);
+            holder.txtTrack = (TextView) view.findViewById(R.id.track_listitem_track);
+            holder.trackColId = c.getColumnIndex(SpotifyContract.TrackEntry.COLUMN_TRACK_NAME);
+            view.setTag(holder);
+
+            return view;
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor cursor) {
+            ViewHolder holder = (ViewHolder) view.getTag();
+
+            if (holder != null) {
+                if (cursor.getString(holder.imageColId).trim().equals("")) {
+                    Picasso.with(context).load(R.drawable.default_image).error(R.drawable.image_download_error).into(holder.imageView);
+                } else {
+                    Picasso.with(context).load(cursor.getString(holder.imageColId)).placeholder(R.drawable.default_image).error(R.drawable.image_download_error).into(holder.imageView);
+                }
+                holder.txtAlbum.setText(cursor.getString(holder.albumColId));
+                holder.txtTrack.setText(cursor.getString(holder.trackColId));
             }
-        }
-
-        @Override
-        public Object getItem(int index) {
-            return mTrackList.get(index);
-        }
-
-        @Override
-        public long getItemId(int index) {
-            return mTrackList.indexOf(getItem(index));
-        }
-
-        @Override
-        public View getView(int index, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-            if (convertView == null) {
-                convertView = inflater.inflate(R.layout.track_listitem, null);
-                holder = new ViewHolder();
-                holder.txtAlbum = (TextView) convertView.findViewById(R.id.track_listitem_album);
-                holder.txtTrack = (TextView) convertView.findViewById(R.id.track_listitem_track);
-                holder.imageView = (ImageView) convertView.findViewById(R.id.track_listitem_image);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            TrackListRow rowItem = (TrackListRow) getItem(index);
-
-            if (rowItem.getImage() != null) {
-                Picasso.with(mContext).load(rowItem.getThumbnail().url).placeholder(R.drawable.default_image).error(R.drawable.image_download_error).into(holder.imageView);
-            }
-            holder.txtAlbum.setText(rowItem.getAlbum());
-            holder.txtTrack.setText(rowItem.getTrack());
-
-            return convertView;
         }
     }
 }
